e3e8f35cf22ae1e519de769bd9501bfb2d1999df
Ali Yürekli <yurekli.ali@gmail.com>
2023-01-10 15:14:04 +0300

    Unit tests for gcd are added

--- src/test/java/com/aliyurekli/primebox/gcd/DivisiveEuclideanGcdTest.java
+++ src/test/java/com/aliyurekli/primebox/gcd/DivisiveEuclideanGcdTest.java
@@ -1,8 +1,23 @@
 package com.aliyurekli.primebox.gcd;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DivisiveEuclideanGcdTest {
+
+    @Test
+    public void testGcd() {
+        Gcd euclidean = new DivisiveEuclideanGcd();
+        int[][] tests = GcdGroundTruth.getGroundTruth();
+
+        for (int[] test: tests) {
+            int num1 = test[0];
+            int num2 = test[1];
+            int expected = test[2];
+
+            Assert.assertEquals(euclidean.greatestCommonDivisor(num1, num2), expected);
+        }
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void testIllegalArgumentException() {
---
+++ src/test/java/com/aliyurekli/primebox/gcd/GcdGroundTruth.java
@@ -0,0 +1,14 @@
+package com.aliyurekli.primebox.gcd;
+
+public class GcdGroundTruth {
+
+    static int[][] getGroundTruth() {
+        return new int[][] {
+                {5, 15, 5},
+                {26, 65, 13},
+                {100, 75, 25},
+                {87, 406, 29},
+                {1085, 117, 1}
+        };
+    }
+}
